b9abd36a7b75348e90913be573b23b083b212239
rmmarcos <r.marcos.1980@gmail.com>
2022-01-11 19:48:55 +0100

    Creacion paquete con funciones de Pere.

---
+++ R/funciones.R
@@ -0,0 +1,28 @@
+#' Calculate NDVI
+#'
+#' This function calculates the normalized difference vegetation index.
+#'
+#' @param b1 value or vector with reflectance in the first (eg. NIR for NDVI) wavelenght
+#' @param b2 value or vector with reflectance in the second (eg. red for NDVI) wavelenght
+#' @return NDVI value
+#' @export
+#'
+ndiff <- function(b1,b2){
+  return((b1-b2)/(b1+b2))
+}
+
+
+#' Calculate nSVAVI
+#'
+#' This function calculates the nSAVI vegetation index.
+#'
+#' @param b1 value or vector with reflectance in the first (eg. NIR for NDVI) wavelenght
+#' @param b2 value or vector with reflectance in the second (eg. red for NDVI) wavelenght
+#' @return nSAVI value
+#' @export
+#'
+nsavi <- function(b1,b2,L=0.5){
+  return((b1-b2)/(b1+b2+L)*(1+L))
+}
+
+
